fc1fc31e5f7521c124b029c34959d9f87f88e3b3
Mike V <vulpes74@proton.me>
2026-07-23 23:58:29 +0400

    added analytics fields, users field is unique'

--- src/models/users.py
+++ src/models/users.py
@@ -1,6 +1,7 @@
 import sqlalchemy as sa
 from sqlalchemy.orm import DeclarativeMeta, Mapped, declarative_base, mapped_column
 
+from datetime import datetime
 from uuid import UUID, uuid4
 
 metadata = sa.MetaData()
@@ -8,6 +9,23 @@ metadata = sa.MetaData()
 
 class BaseServiceModel:
     """Базовый класс для таблиц сервиса."""
+
+    created_at: Mapped[datetime] = mapped_column(
+        sa.DateTime(timezone=True),
+        server_default=sa.func.now(),
+        nullable=False,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        sa.DateTime(timezone=True),
+        server_default=sa.func.now(),
+        onupdate=sa.func.now(),
+        nullable=False,
+    )
+    is_deleted: Mapped[bool] = mapped_column(
+        sa.Boolean(),
+        server_default=sa.false(),
+        nullable=False,
+    )
 
     @classmethod
     def on_conflict_constraint(cls) -> tuple | None:
@@ -20,4 +38,4 @@ Base: DeclarativeMeta = declarative_base(metadata=metadata, cls=BaseServiceModel
 class UserModel(Base):
     __tablename__ = 'users'
     id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
-    username: Mapped[str] = mapped_column(sa.String())
+    username: Mapped[str] = mapped_column(sa.String(), unique=True)
